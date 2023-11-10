71caee13a6cd936dec3aca3d9b335783c4d7cd90
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-10 17:46:23 -0500

    Feat : basic structure of all the commponents and navigation elements for my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,53 @@ import { useState } from 'react'
 import './App.css'
 import AboutMe from './components/AboutMe'
 import Header from './components/Header'
+import styled from "styled-components"
+import NyanCatProject from "./assets/NyanCatImg.gif"
+import FruitStoreProject from "./assets/FruitStoreWorkshop.png"
+import CookieClickerProject from "./assets/CookieClickerProject.png"
+
+
+//Nic eportfolio exammple : https://sharon-yi.com///
 
 function App() {
 
   return (
     <>
       <h1> HI MY NAME IS CHARLES-ERIC </h1>
+
+      <div>
+      <h2>I'm a passionate developper that like to work hard and build nices projects</h2>
+      <h3>If you need a project well done, with passion and good vibes, I'm your dev !</h3>
+      </div>
+
+      <h2>Tools I work with</h2>
+      //List of all the icons I work with will go here ... ...//
+
+      <h2>Down here you can see all my current projects...</h2>
+      <ProjectGridContainer>
+        <img src={NyanCatProject} alt= "Image of my NyanCat project"></img>
+        <img src={FruitStoreProject} alt= "Image of my Fruit Store webPage"></img>
+        <img src={CookieClickerProject} alt= "Image of my Cookie Clicker game"/>
+      </ProjectGridContainer>
+
+      <h2>Get in Touch</h2>
+      LinkedIN Logo (With Link)
+      GitHub LOgo (with Link)
+      
+      
     </>
   )
 }
 
 export default App
+
+const ProjectGridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+
+  img {
+    width:100%;
+  }
+
+`
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 
+//Scroll to : https://stackoverflow.com/questions/54715462/react-scroll-how-to-scroll-to-a-specific-targeted-component-when-clicking-on-n
 
 const Header = () => {
 
@@ -7,6 +8,7 @@ const Header = () => {
   // and get in touch should be in the same page
     return (
       <>
+            <NavLink to="/">Home</NavLink>
             <NavLink to="/Aboutme">About Me</NavLink>
             <NavLink>Projects</NavLink>
             <NavLink>Get In Touch</NavLink>
