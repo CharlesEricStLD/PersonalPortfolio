2a52016e29a43adcc9440780a87e81a3d40c556d
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-12-07 18:37:51 -0500

    Fix : remove unused component

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import AboutMe from './components/AboutMe.jsx'
-import Header from './Components/Header.jsx'
 import { BrowserRouter } from "react-router-dom";
 import {Routes} from "react-router-dom";
 import {Route} from "react-router-dom";
-import ScrollToAnchor from './Components/ScrollToAnchor.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
     <BrowserRouter>
-    <ScrollToAnchor/>
     <Routes>
         <Route path="/" element ={<App/>}> </Route>
         <Route path="/AboutMe" element={<AboutMe/>}></Route> 
