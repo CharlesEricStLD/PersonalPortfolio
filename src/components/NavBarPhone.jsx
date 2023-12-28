b06ec9c4fcf25e32b6dc3c8d56688756335fc00a
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-12-28 14:15:40 -0500

    Fix : Make phone drawer clickable and change icon

--- src/components/NavBarPhone.jsx
+++ src/components/NavBarPhone.jsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
@@ -11,11 +13,16 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import styled from 'styled-components'
+import ComputerIcon from '@mui/icons-material/Computer';
+import HomeIcon from '@mui/icons-material/Home';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import EmailIcon from '@mui/icons-material/Email';
 
 const NavBarPhone = () => {
-  const [state, setState] = React.useState({
-    right: false,
-  });
+
+  const [userOnHomePage] = useState(location.pathname === "/");
+
+  const [state, setState] = useState({right: false});
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -25,25 +32,64 @@ const NavBarPhone = () => {
     setState({ ...state, [anchor]: open });
   };
 
+  function ListItemLink(props) {
+    const { icon, primary, to } = props;
+  
+    return (
+      <List>
+        <ListItem button component={Link} to={to}>
+          {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+          <ListItemText primary={primary} />
+        </ListItem>
+      </List>
+    );
+  }
+
+  function ListItemAnchor(props) {
+    const { icon, primary, href } = props;
+  
+    return (
+        <List>
+        <ListItem button component="a" href={href}>
+          {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+          <ListItemText primary={primary} />
+        </ListItem>
+        </List>
+    );
+  }
+
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250, background:"linear-gradient(#0f2030, #b0d6e6be)"}}
+      sx={{ width: 'auto', background:"linear-gradient(#0f2030, #718994bd)", fontFamily:"LarkenExtraBold", color:"#fff3af"}}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
+      {userOnHomePage ? 
       <List>
-        {['Home', 'About Me', 'Projects', 'Get In Touch'].map((text, index) => (
+        {['Home', 'About Me', 'Projects', 'Get In Touch'].map(text => (
           <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
+          <>
+          {text === "Home" && <ListItemLink to="/" primary={text} icon={<HomeIcon/>} />}
+
+          {text === "Projects" && <ListItemAnchor href="#projectsSection" primary={text} icon={<ComputerIcon/>}/>}
+
+          {text === "About Me" && <ListItemLink  to="/Aboutme" primary={text} icon={<EmojiPeopleIcon/>} />}
+          
+          {text === "Get In Touch" && <ListItemAnchor href="#getInTouchSection" primary={text} icon={<EmailIcon/>}/>}
+        
+        </>
           </ListItem>
         ))}
       </List>
+      : 
+      <List>
+        <ListItem disablePadding>
+        <ListItemLink to="/" primary={"Get Back Home"} icon={<HomeIcon/>} />
+        </ListItem>
+      </List>
+      } 
+        
       <Divider />
     </Box>
   );
