c74db1985ebf9cfb628b22b01011cf825f85ca4b
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-06-02 12:06:45 -0400

    Feat : add a new Route for the page projects/CrossCountryFinder

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
         <Route path="/" element ={<App/>}> </Route>
-        <Route path="/AboutMe" element={<AboutMe/>}></Route> 
+        <Route path="/AboutMe" element={<AboutMe/>}></Route>
+        <Route path="/projects/CrossCountryFinder" element={<CrossCountryFinder/>}></Route>
     </Routes>
     </BrowserRouter>
     </>
