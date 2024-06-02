3d0dee49db8793711a0dee70be675a53e24fb4a1
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-06-02 12:23:27 -0400

    Feat : Create ProjectPage component without styling

---
+++ src/components/ProjectsPage.jsx
@@ -0,0 +1,40 @@
+//Component of the fdescription of Project Page
+
+//Title
+// Read time
+//DescriptionOfProject (2 lines max)
+//Type of Project
+// Stack use/Technologies
+// If project is live
+//Project Purpose and Goal
+//Web Stack and Explanation
+//Problems and Thought Process
+
+const ProjectsPage = () => {
+
+  return (
+    <ProjectsPage>
+    <h1>{nameOfProject}</h1>
+    <p>{readTime}</p>
+    <p>{SmalldescriptionOfProject}</p>
+    {/* table of 3 columns */}
+    <h2>Type</h2>
+    <h2>Stack</h2>
+    <h2>Live</h2>
+    {/* End of table of 3 columns */}
+    {/* ScreenShot of the app*/}
+    <img>{screenShot1}</img>
+    <h2>Project Purpose and Goal</h2>
+    <p>{projectPurposeAndGoal}</p>
+    <h2>Web Stack and Explanation</h2>
+    <p>{WebStackAndExplanation}</p>
+    <h2>Problems and Thought Process</h2>
+    <p>{ProblemsAndToughtProcess}</p>
+    <h2>Lessons Learned & Future Work</h2>
+    <p>{lessonsLearned}</p>    
+    </ProjectsPage>
+  )
+
+
+}
+
