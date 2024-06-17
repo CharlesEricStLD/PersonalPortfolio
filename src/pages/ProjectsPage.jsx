2a9624e98ee85acdf99758f70cd88bfbe27d1be6
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-06-17 12:29:04 -0400

    Feat : Add show more button

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import crossCountryFinderScreenShot from  "../../public/ScreenShot1CrossCountryFinder.png"
-
+import { useState } from "react";
+import Header from "../components/Header";
 //Component of the description of Project Page
 //Alejandro model : https://www.aaspinwall.com/projects/ing
 
@@ -24,11 +24,15 @@ import crossCountryFinderScreenShot from  "../../public/ScreenShot1CrossCountryF
 // ["My first problem was that there wasn't any database of all the crossCountry centers in Quebec, So my first job was to extract much as I can from other website or blog and confirm that the center still exist. To assess this problem I decided to use Pupeteer and it's automation and scraping functionality. I first write a script to scrape an already, but very outdated database on the web and check if the web link was still inactive (don't end-up dead or give me a 404 page). After that, I need to get the address, phone and email of each center. So I create a second pupeteer script to automate a search on Google and to scrape the data return by the script.", "When I get my database validate and import into Mongo DB, I need to find the snow condition of each center. Some condition are on the home page, some condition are in a specific url on the website. For this part, I decide to divide my task in multiple group. First one was to create a function for the website host by the same server platform and to create a function for each of them. After that, for the rest that were too much different I did it manually by going throuigh each site and finding the html element to scrape to find the condition. After that I can just run my Pupeteer script to scrape all my website specific html element each day","My third big problem was to uniformise all my data that I sccrape from the web, because what is the goal of using a databsee of condition, if it's not faster than going into the website directly. To do that I decide to use Open AI API to parse my data. I first think about the questions I wanted answer in my condition report and what was the best way to display my data. I decide to ask these questions :  Is it open or closed? Any closed tracks, snow conditions, or important warnings and what is the last updated date? I decide to present it in a table format for ease of understanding and efficiency. Finaly, to be sure that I always receive from the API the same format of answer, I ask the Open AI API to return me a Json format and I add a null answer if the AI can't find the answer"] 
 //
 
-const allStack = CrossCountryProject.stacks;
-
 
 export const ProjectsPage = ({project}) => {
+  let shortStacks = [];
+  shortStacks = project.stacks.slice(0,3);
+  const [showMore, setShowMore] = useState(false);
+
   return (
+    <>
+    <Header/>
     <ProjectDescription>
     <h1>{project.nameOfProject}</h1>
     <p>{project.readTime}</p>
@@ -41,12 +45,21 @@ export const ProjectsPage = ({project}) => {
     </div>
     <div>
     <h2>Stack</h2>
-    {
-    project.stacks.map(stack => (
-    <p>{stack}</p>
-    ))
-    }
+    
+    {/* Affichage conditionnel des stacks */}
+    {project.stacks.length > 3 && !showMore ? (
+        shortStacks.map(stack => <p key={stack}>{stack}</p>)
+      ) : (
+        project.stacks.map(stack => <p key={stack}>{stack}</p>)
+      )}
+      
+      {/* Bouton pour basculer entre 'Show more' et 'Show less' */}
+      {<ShowMoreButton onClick={() => setShowMore(!showMore)}>
+          {showMore ? "Show less" : "Show more"}
+        </ShowMoreButton>
+      }
     </div>
+
     <div>
     {project.githubLink ? 
     <><h2>Github</h2><p> <a href="{project.githubLink}">GitHub</a> </p></>: <> <h2>Live</h2> <p>{project.liveLink}</p> </>}
@@ -69,6 +82,7 @@ export const ProjectsPage = ({project}) => {
     <h2>Lessons Learned & Future Work</h2>
     <p>{project.lessonsLearned}</p>    
     </ProjectDescription>
+    </>
   )
 }
 
@@ -86,4 +100,18 @@ p {
 const ScreenShot = styled.img`
   max-width:100%;
   max-height: auto;
+`
+
+const ShowMoreButton = styled.button`
+  background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+  color: rgb(158, 158, 255);
+
+  :focus {
+    outline: revert;
+  }
 `
