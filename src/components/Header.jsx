6492ec4616f85d6cbdc883225d746582f5e08a96
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-07-24 13:31:09 -0400

    Feat : remove NavBar from Header and add styling for Phone Nav Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import NavBarPhone from "./NavBarPhone";
 import { useState } from "react";
 
-const Header = () => {
+export const Header = () => {
   
   const location = useLocation();
 
@@ -11,17 +10,12 @@ const Header = () => {
   const [userOnHomePage] = useState(location.pathname === "/");
 
   return (
-      <>
-        <MenuHiding>
-        <NavBarPhone/>
-        </MenuHiding>
         <HeaderContainer $useronhomepage={userOnHomePage}>
             {userOnHomePage ? <NavItems to="/"> Home</NavItems> : <NavItems to="/"> Back to Home</NavItems>}
             {userOnHomePage ? <NavItems to="/Aboutme"> About Me</NavItems> : null}
             {userOnHomePage ? <a className="header" href="#projectsSection">Projects</a> : null }
             {userOnHomePage ? <a className="header" href="#getInTouchSection">Get In Touch</a> : null}
           </HeaderContainer>
-      </>   
     ) 
   } 
   
@@ -47,54 +41,49 @@ const Header = () => {
       font-size: 0.9em;
     }
     //Phone view
-    @media ( min-width: 320px) and (max-width:579px) {
-      display: none;
+  @media ( min-width: 320px) and (max-width:579px) {
+      font-size  : 1em;
+      position: fixed;
+      inset : 0 0 0 50%;
+      display: flex;
+      flex-direction: column;
+      gap:2em;  
+      padding:1em 0 0 1em; 
+      background-color: hsl(100 0 0%/0.1);
+      backdrop-filter: blur(1em);
     }
   `
-
-  const MenuHiding = styled.div`
-    display:none;
-
-    //tablet view
-    @media ( min-width: 580px) and (max-width:700px) {
-      display:none;
-    }
-    //Phone view
-    @media ( min-width: 320px) and (max-width:579px) {
-      display: block;
-    }
-  `
-
 const NavItems = styled(NavLink)`
-    margin-right:5%;
+margin-right:5%;
 
 & {
-  color: #646cff;
-  text-decoration: inherit;
-  font-family: "LarkenExtraBold";
+color: #646cff;
+text-decoration: inherit;
+font-family: "LarkenExtraBold";
 }
 
 &::after {
-  content: '';
-  display: block;
-  margin: auto;
-  width: 0;
-  height: 3px;
-  background: #fff3af;
-  border-radius: 5px;
+content: '';
+display: block;
+margin: auto;
+width: 0;
+height: 3px;
+background: #fff3af;
+border-radius: 5px;
 }
 
 @keyframes borderAnimation{
-  0%   {width:0;}
-  100% {width:100%;}
+0%   {width:0;}
+100% {width:100%;}
 }
 
 
 &:hover::after {
-  animation: borderAnimation 1s;;
-  width: 100%;
+animation: borderAnimation 1s;;
+width: 100%;
 }
-  `
+`
+
 
 
 
