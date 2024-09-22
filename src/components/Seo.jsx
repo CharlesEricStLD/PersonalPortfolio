de5b58cc0232a4282e0cdede5fac0d3e9baa878a
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-09-22 09:11:48 -0400

    modified image tag in meta tags

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import portfolioImage from "../public/portfolioMetaImage.png"
-
 const Seo = ({title, description, type, url}) => {
   return (
   <Helmet>
@@ -13,7 +11,7 @@ const Seo = ({title, description, type, url}) => {
   <meta property="og:type" content={type} />
   <meta property="og:url" content={url} />
   <meta property="og:title" content={title} />
-  <meta property="og:image" content={portfolioImage} />
+  <meta property="og:image" content="https://cstlouidupuis.com/portfolioMetaImage.png"/>
   <meta property="og:image:width" content="1920" />
   <meta property="og:image:height" content="1520" />
 
@@ -22,7 +20,7 @@ const Seo = ({title, description, type, url}) => {
   { /* Twitter tags */ }
   <meta name="twitter:title" content={title}/>
   <meta name="twitter:description" content={description}/>
-  <meta name="twitter:image" content={portfolioImage}/>
+  <meta name="twitter:image" content="https://cstlouidupuis.com/portfolioMetaImage.png"/>
   { /* End Twitter tags */ }
   </Helmet>
   )
