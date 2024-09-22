cc3862e2840c819bd697faa488d12a333ac7858c
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-09-22 08:06:06 -0400

    Add SEO Image of website and update meta tags of seo component

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import portfolioImage from "../assets/portfolioMetaImage.png"
 
-export const SEO = ({title, description, name, type}) => {
+export const SEO = ({title, description, type, url}) => {
   return (
   <Helmet>
   { /* Standard metadata tags */ }
-  <title>{title}</title>
-  <meta name='description' content={description} />
+  {<meta name="description" content={description}/>}
+  {<meta name="author" content="Charles-Eric St-Louis-Dupuis"/>}
   { /* End standard metadata tags */ }
   { /* Facebook tags */ }
   <meta property="og:type" content={type} />
+  <meta property="og:url" content={url} />
   <meta property="og:title" content={title} />
+  <meta property="og:image" content={portfolioImage} />
   <meta property="og:description" content={description} />
   { /* End Facebook tags */ }
   { /* Twitter tags */ }
-  <meta name="twitter:creator" content={name} />
-  <meta name="twitter:card" content={type} />
-  <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content={description} />
+  <meta name="twitter:title" content={title}/>
+  <meta name="twitter:description" content={description}/>
+  <meta name="twitter:image" content={image}/>
   { /* End Twitter tags */ }
   </Helmet>
   )
   }
+
