29fb02cd6e10026e01678404dcef9f23acba6e00
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2024-09-23 09:23:13 -0400

    Add cloudinary module and code to render image

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import {fill} from "@cloudinary/url-gen/actions/resize";
+import {Cloudinary} from "@cloudinary/url-gen";
+
 const Seo = ({title, description, type, url}) => {
+
+  
+// Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'dpz0d94hw'
+  }
+});
+
+// Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+const myImage = cld.image('portfolioMetaImage_z9nbot'); 
+
+const portfolioImageSrc = myImage.resize(fill().width(250).height(250)).toURL();
+
   return (
   <Helmet>
   { /* Standard metadata tags */ }
@@ -11,7 +28,7 @@ const Seo = ({title, description, type, url}) => {
   <meta property="og:type" content={type} />
   <meta property="og:url" content={url} />
   <meta property="og:title" content={title} />
-  <meta property="og:image" content="https://cstlouidupuis.com/portfolioMetaImage.png"/>
+  <meta property="og:image" content={portfolioImageSrc}/>
   <meta property="og:image:width" content="1920" />
   <meta property="og:image:height" content="1520" />
 
@@ -20,7 +37,7 @@ const Seo = ({title, description, type, url}) => {
   { /* Twitter tags */ }
   <meta name="twitter:title" content={title}/>
   <meta name="twitter:description" content={description}/>
-  <meta name="twitter:image" content="https://cstlouidupuis.com/portfolioMetaImage.png"/>
+  <meta name="twitter:image" content={portfolioImageSrc}/>
   { /* End Twitter tags */ }
   </Helmet>
   )
